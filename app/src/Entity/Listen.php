254f08f74221909fa24eeece3e526520e8b2ed8d
jlieth <jlieth@users.noreply.github.com>
2022-04-04 01:13:32 +0200

    removed redundant fields from Listen entity

--- app/src/Entity/Listen.php
+++ app/src/Entity/Listen.php
@@ -6,7 +6,7 @@ use App\Repository\ListenRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ListenRepository::class)]
-#[ORM\UniqueConstraint(name: "profile_timestamp", columns: ["profile_id", "timestamp"])]
+#[ORM\UniqueConstraint(name: "profile_date", columns: ["profile_id", "date"])]
 class Listen
 {
     #[ORM\Id]
@@ -20,27 +20,6 @@ class Listen
     #[ORM\ManyToOne(targetEntity: Profile::class, inversedBy: "listens")]
     #[ORM\JoinColumn(nullable: false)]
     private $profile;
-
-    #[ORM\Column(type: "integer")]
-    private $timestamp;
-
-    #[ORM\Column(type: "string", length: 1024)]
-    private $artistName;
-
-    #[ORM\Column(type: "string", length: 1024, nullable: true)]
-    private $albumArtistName;
-
-    #[ORM\Column(type: "string", length: 1024, nullable: true)]
-    private $albumTitle;
-
-    #[ORM\Column(type: "string", length: 1024)]
-    private $trackTitle;
-
-    #[ORM\Column(type: "integer")]
-    private $length;
-
-    #[ORM\Column(type: "integer", nullable: true)]
-    private $tracknumber;
 
     #[ORM\ManyToOne(targetEntity: Artist::class, inversedBy: "listens")]
     #[ORM\JoinColumn(nullable: false)]
@@ -79,90 +58,6 @@ class Listen
     public function setProfile(?Profile $profile): self
     {
         $this->profile = $profile;
-
-        return $this;
-    }
-
-    public function getTimestamp(): ?int
-    {
-        return $this->timestamp;
-    }
-
-    public function setTimestamp(int $timestamp): self
-    {
-        $this->timestamp = $timestamp;
-
-        return $this;
-    }
-
-    public function getArtistName(): ?string
-    {
-        return $this->artistName;
-    }
-
-    public function setArtistName(string $artistName): self
-    {
-        $this->artistName = $artistName;
-
-        return $this;
-    }
-
-    public function getAlbumArtistName(): ?string
-    {
-        return $this->albumArtistName;
-    }
-
-    public function setAlbumArtistName(?string $albumArtistName): self
-    {
-        $this->albumArtistName = $albumArtistName;
-
-        return $this;
-    }
-
-    public function getAlbumTitle(): ?string
-    {
-        return $this->albumTitle;
-    }
-
-    public function setAlbumTitle(string $albumTitle): self
-    {
-        $this->albumTitle = $albumTitle;
-
-        return $this;
-    }
-
-    public function getTrackTitle(): ?string
-    {
-        return $this->trackTitle;
-    }
-
-    public function setTrackTitle(string $trackTitle): self
-    {
-        $this->trackTitle = $trackTitle;
-
-        return $this;
-    }
-
-    public function getLength(): ?int
-    {
-        return $this->length;
-    }
-
-    public function setLength(int $length): self
-    {
-        $this->length = $length;
-
-        return $this;
-    }
-
-    public function getTracknumber(): ?int
-    {
-        return $this->tracknumber;
-    }
-
-    public function setTracknumber(?int $tracknumber): self
-    {
-        $this->tracknumber = $tracknumber;
 
         return $this;
     }
