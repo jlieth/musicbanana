cc0236a2f39e198bfbfd07be02c8dee313ace254
jlieth <jlieth@users.noreply.github.com>
2022-04-12 09:19:56 +0200

    install npm into image + dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,3 +4,6 @@ docker run --rm --interactive --tty \
     --volume "$PWD/app:/app" \
     --user "$(id -u):$(id -g)" \
     composer:2 install
+
+docker-compose run --rm --user "$(id -u):$(id -g)" musicbanana \
+    sh -c "npm_config_cache=$(mktemp -d) npm install"
