1f25c9e42b1cccd8b79878bbe8f30ad58790b883
jlieth <jlieth@users.noreply.github.com>
2022-05-02 21:24:00 +0200

    flashes in own component; css fix

---
+++ app/assets/js/Components/Flashes.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { page } from "@inertiajs/inertia-svelte"
+    import { BorderAlert } from "flowbite-svelte"
+
+    $: flash = $page.props.flash
+</script>
+
+<div class="flash">
+    {#each flash.error as error}
+    <BorderAlert alertId="border-alert-2" color="red" closeBtn>
+        {error}
+    </BorderAlert>
+    {/each}
+
+    {#each flash.success as success}
+    <BorderAlert alertId="border-alert-2" color="green" closeBtn>
+        {success}
+    </BorderAlert>
+    {/each}
+</div>
--- app/assets/js/Layout.svelte
+++ app/assets/js/Layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { page } from "@inertiajs/inertia-svelte"
-    import { BorderAlert } from "flowbite-svelte";
+    import Flashes from "@/Components/Flashes.svelte"
     import Header from "@/Components/Header/Header.svelte"
 
     $: console.log($page.props)
@@ -10,18 +10,7 @@
 <Header />
 <div id="wrapper">
     <slot name="flash">
-        <div class="flash">
-            {#each flash.error as error}
-            <BorderAlert alertId="border-alert-2" color="red" closeBtn>
-                {error}
-            </BorderAlert>
-            {/each}
-            {#each flash.success as success}
-            <BorderAlert alertId="border-alert-2" color="green" closeBtn>
-                {success}
-            </BorderAlert>
-            {/each}
-        </div>
+        <Flashes />
     </slot>
     <slot name="content" />
 </div>
@@ -33,7 +22,6 @@
 
     #wrapper {
         @apply max-w-xxl mx-auto my-2 md:my-0 p-5;
-        @apply flex flex-row md:flex-col;
         @apply border border-gray-500 border-opacity-50 md:border-0;
         @apply bg-gray-100;
     }
