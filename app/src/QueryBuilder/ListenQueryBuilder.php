1ec8bf612bff1f4726220281e64f614b9e07bb6b
jlieth <jlieth@users.noreply.github.com>
2022-05-21 22:45:26 +0200

    join profile table if not yet joined

--- app/src/QueryBuilder/ListenQueryBuilder.php
+++ app/src/QueryBuilder/ListenQueryBuilder.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\QueryBuilder;
 
-use Doctrine\DBAL\Connection;
 use App\Entity\User;
+use Doctrine\DBAL\Connection;
 
 class ListenQueryBuilder extends BaseQueryBuilder {
-    public function filterByUser(User $user) {
+    public function filterByUser(User $user): static
+    {
         // get all profile ids belonging to this user
         $profileIds = [];
         foreach ($user->getProfiles() as $profile) {
@@ -20,8 +21,18 @@ class ListenQueryBuilder extends BaseQueryBuilder {
         return $this;
     }
 
-    public function public() {
-        $this->andWhere("p.is_public = :_public");
+    public function public(): static
+    {
+        $profileTable = self::TABLE_NAMES["profile"];
+
+        // join profile table if not yet joined
+        $isJoined = in_array($profileTable, array_keys($this->joins));
+        if (!$isJoined) {
+            $this->innerJoin("l", $profileTable, "p", "l.profile_id = p.id");
+        }
+
+        $alias = $this->joins[$profileTable];
+        $this->andWhere("$alias.is_public = :_public");
         $this->setParameter("_public", true);
         return $this;
     }
