814e2bb870a87bdca822af35db8e94ef66edcbe5
jlieth <jlieth@users.noreply.github.com>
2022-05-21 22:43:15 +0200

    keep track of joined tables

--- app/src/QueryBuilder/BaseQueryBuilder.php
+++ app/src/QueryBuilder/BaseQueryBuilder.php
@@ -8,8 +8,27 @@ use Doctrine\DBAL\Query\QueryBuilder;
 
 abstract class BaseQueryBuilder extends QueryBuilder {
     const ITEMS_PER_PAGE = 10;
+    const TABLE_NAMES = [
+        "album" => "Album",
+        "artist" => "Artist",
+        "listen" => "Listen",
+        "profile" => "Profile",
+        "track" => "Track",
+        "user" => "usr",
+    ];
 
-    public function page(int $page = 1) {
+    protected array $joins = [];
+
+    public function innerJoin($fromAlias, $join, $alias, $condition = null): static
+    {
+        // keep track of joined tables
+        $this->joins[$join] = $alias;
+
+        return parent::innerJoin($fromAlias, $join, $alias, $condition);
+    }
+
+    public function page(int $page = 1): static
+    {
         $offset = ($page - 1) * self::ITEMS_PER_PAGE;
 
         $this->setFirstResult($offset);
