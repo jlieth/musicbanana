801a5bd146e8b24ca1a38b4f336cee03daef5bf5
jlieth <jlieth@users.noreply.github.com>
2022-12-03 12:46:13 +0100

    breakpoint for tablet devices

--- app/assets/js/Pages/_Layout.svelte
+++ app/assets/js/Pages/_Layout.svelte
@@ -16,7 +16,7 @@
     }
 
     #wrapper {
-        @apply w-1/2 md:w-full max-w-xxl mx-auto my-2 md:my-0;
+        @apply w-[1320px] xl:w-[95%] md:w-full max-w-xxl mx-auto my-2 md:my-0;
         @apply border border-gray-500 border-opacity-50 md:border-0;
         @apply bg-gray-100;
     }
